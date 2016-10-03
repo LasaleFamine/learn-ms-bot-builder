a2ed8ba4b3a08470d7da3bf0918e5876fabe5465
LasaleFamine <info@godev.space>
2016-10-03 17:44:26 +0200

    re-fix #4 and add test for not found

--- test/test.js
+++ test/test.js
@@ -10,6 +10,13 @@ test('getGithubUser()', async t => {
   t.deepEqual(userDetails.login, 'LasaleFamine', 'correct login name from github')
 })
 
+test('getGithubUser() (not found)', async t => {
+  const userDetails = await getGithubUser('notfoundofcourses')
+  const objNotFound = {message: 'Not Found', error: true}
+  t.deepEqual(userDetails, objNotFound, 'Correct not found hanlder')
+  // t.deepEqual(userDetails.message, 'Not found', 'correct not found message')
+})
+
 test('model user()', async t => {
   const userDetails = await getGithubUser('lasalefamine')
   const modelUser = user(userDetails)
@@ -22,7 +29,7 @@ following: 31,
 publicRepos: 27,
 publicGits: 4,
 created: 2015/06/26,
-reposUrl: https://api.github.com/users/LasaleFamine/repos
+reposUrl: https://api.github.com/users/LasaleFamine/repos 
   `
   t.truthy(modelUser, 'User exists with details')
   t.deepEqual(modelUser, msg, 'correct information for the user')
